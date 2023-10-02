7aa7a8a6002dbdd76932cc4f62e97740291db683
aocaranza77 <alejandro.ocaranza.2016@gmail.com>
2023-10-02 01:44:45 +0000

    commit

--- src/workflow-final/601_RUN_correr_workflow_08.r
+++ src/workflow-final/601_RUN_correr_workflow_08.r
@@ -1,4 +1,4 @@
-# Corrida general del workflow
+# Corrida general del workflow final
 
 options(error = function() {
   traceback(20)
@@ -10,11 +10,11 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-source("~/labo2023ba/src/workflow-inicial/611_CA_reparar_dataset_08.r")
-source("~/labo2023ba/src/workflow-inicial/621_DR_corregir_drifting_08.r")
-source("~/labo2023ba/src/workflow-inicial/631_FE_historia_08.r")
-source("~/labo2023ba/src/workflow-inicial/641_TS_training_strategy_08.r")
+source("~/labo2023ba/src/workflow-final/611_CA_reparar_dataset_08.r")
+source("~/labo2023ba/src/workflow-final/621_DR_corregir_drifting_08.r")
+source("~/labo2023ba/src/workflow-final/631_FE_historia_08.r")
+source("~/labo2023ba/src/workflow-final/641_TS_training_strategy_08.r")
 
 # ultimos pasos, muy lentos
-source("~/labo2023ba/src/workflow-inicial/651_HT_lightgbm_08.r")
-source("~/labo2023ba/src/workflow-inicial/661_ZZ_final_08.r")
+source("~/labo2023ba/src/workflow-final/651_HT_lightgbm_08.r")
+source("~/labo2023ba/src/workflow-final/661_ZZ_final_08.r")
