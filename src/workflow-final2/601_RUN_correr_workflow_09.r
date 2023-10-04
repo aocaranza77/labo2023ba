ea265e8f9d3c289828b543fc601ed41c1cdcc686
aocaranza77 <alejandro.ocaranza.2016@gmail.com>
2023-10-04 01:46:03 +0000

    commit

--- src/workflow-final2/601_RUN_correr_workflow_09.r
+++ src/workflow-final2/601_RUN_correr_workflow_09.r
@@ -10,11 +10,11 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-source("~/labo2023ba/src/workflow-final/611_CA_reparar_dataset_08.r")
-source("~/labo2023ba/src/workflow-final/621_DR_corregir_drifting_08.r")
-source("~/labo2023ba/src/workflow-final/631_FE_historia_08.r")
-source("~/labo2023ba/src/workflow-final/641_TS_training_strategy_08.r")
+source("~/labo2023ba/src/workflow-final2/611_CA_reparar_dataset_09.r")
+source("~/labo2023ba/src/workflow-final2/621_DR_corregir_drifting_09.r")
+source("~/labo2023ba/src/workflow-final2/631_FE_historia_09.r")
+source("~/labo2023ba/src/workflow-final2/641_TS_training_strategy_09.r")
 
 # ultimos pasos, muy lentos
-source("~/labo2023ba/src/workflow-final/651_HT_lightgbm_08.r")
-source("~/labo2023ba/src/workflow-final/661_ZZ_final_08.r")
+source("~/labo2023ba/src/workflow-final2/651_HT_lightgbm_09.r")
+source("~/labo2023ba/src/workflow-final2/661_ZZ_final_09.r")
