b125757591fafe1970dbba37d15e9aa6ad2f146e
aocaranza77 <alejandro.ocaranza.2016@gmail.com>
2023-10-04 02:17:40 +0000

    commit

--- src/workflow-final2/661_ZZ_final_09.r
+++ src/workflow-final2/661_ZZ_final_09.r
@@ -1,4 +1,4 @@
-# Experimentos Colaborativos Default
+# Experimentos Colaborativos final
 
 # Workflow  ZZ proceso final con semillas
 # Este nuevo script fue desarrolado para ayudar a los alumnos en la realizacion
